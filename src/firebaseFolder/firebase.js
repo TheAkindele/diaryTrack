96e075cf8e0fa023c6530be96e3ff9427842bcd1
Akindele Muhyideen <disgcfrguy@gmail.com>
2020-06-03 12:12:01 +0100

    modified the firebase settings

--- src/firebaseFolder/firebase.js
+++ src/firebaseFolder/firebase.js
@@ -14,8 +14,11 @@ var firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
-export const firestore = firebase.firestore()
-export const auth = firebase.auth()
+firebase.firestore()
+const auth = firebase.auth()
 
+const provider = new firebase.auth.GoogleAuthProvider()
+provider.setCustomParameters({ prompt: 'select_account' })
+export const signInWithGoogle = () => auth.signInWithPopup(provider)
 
 export default firebase
