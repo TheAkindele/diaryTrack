f1811dd72aee61a415a75d3e4d9d7018d5c354cc
Akindele Muhyideen <disgcfrguy@gmail.com>
2020-06-03 12:25:17 +0100

    edited the sign in and sign out import error

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import NavBar from './components/navFolder/Navbar'
 import Dashboard from './components/homepageFolder/dashboardFolder/Dashboard'
 import Event from './components/eventFolder/Event'
 import NewEvent from './components/createEventFolder/NewEvent'
-import SignIn from './components/signIn&OutFolder/SignIn'
-import SignUp from './components/signIn&OutFolder/SignUp'
+import SignIn from './components/signInAndOutFolder/SignIn'
+import SignUp from './components/signInAndOutFolder/SignUp'
 import './App.scss'
 
 function App() {
